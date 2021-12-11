35c72219b8b426e71a235a3c898165ae8b9e38dc
Ray Guo <ihahoo@163.com>
2021-12-12 02:46:02 +0800

    增加配置文件支持

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,30 @@ use actix_web::{http, web, App, HttpServer, Result, HttpResponse};
 use lib::error;
 use routes::hello;
 
+#[derive(Clone)]
+pub struct AppState {
+    pub config: config::Config,
+}
+
 async fn index() -> Result<web::HttpResponse, error::Error> {
     Ok(HttpResponse::Ok().body(""))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let mut settings = config::Config::default();
+    settings.merge(config::File::with_name("data/config/app.toml")).unwrap();
+    let port = settings.get::<String>("app.port").unwrap();
+
+    HttpServer::new(move || {
         let cors = Cors::permissive();
 
-        println!("==> 🚀 {} listening at {}", "app", "8080");
+        println!("==> 🚀 {} listening at {}", "app", settings.get::<String>("app.port").unwrap());
 
         App::new()
+            .data(AppState {
+                config: settings.clone(),
+            })
             .wrap(
                 ErrorHandlers::new()
                     .handler(http::StatusCode::METHOD_NOT_ALLOWED, error::render_405)
@@ -31,7 +43,7 @@ async fn main() -> std::io::Result<()> {
             .configure(hello::route)
             .service(web::resource("/").route(web::get().to(index)))
     })
-    .bind("0.0.0.0:8080")?
+    .bind(format!("0.0.0.0:{}", port))?
     .run()
     .await
 }
